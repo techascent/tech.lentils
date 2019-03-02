6b0942cbd4fb613ec3fad6be096e5973819bab22
Chris Nuernberger <chris@thinktopic.com>
2019-03-02 14:53:21 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.lentils "0.01"
+(defproject techascent/tech.lentils "0.02-SNAPSHOT"
   :description "Bindings of the intel daal library into the techascent ecosystem."
   :url "http://github.com/techascent/tech.lentils"
   :license {:name "Eclipse Public License"
